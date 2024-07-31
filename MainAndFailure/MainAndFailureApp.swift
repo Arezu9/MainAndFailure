ffc4e40ae32a449a45b6781d316f7f2aa7f350cb
Arezoo <arezooinfo883@gmail.com>
2024-07-31 10:40:35 +0100

    Initial Commit

---
+++ MainAndFailure/MainAndFailureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAndFailureApp.swift
+//  MainAndFailure
+//
+//  Created by Scholar on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MainAndFailureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
